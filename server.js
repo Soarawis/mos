4b77ad4b1f749b61250c2f06c8cc9ba67993e899
Soarawis <268345221021-st@rmutsb.ac.th>
2026-04-27 15:08:43 +0700

    fix

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// หน้าแรก
 app.get("/", (req, res) => {
   res.send("API is running 🚀");
 });
 
-// สำคัญมากสำหรับ Railway
+// API ทดสอบ
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API ใช้งานได้" });
+});
+
+// สำคัญสำหรับ Railway
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
